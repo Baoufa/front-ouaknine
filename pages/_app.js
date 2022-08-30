64b0aa9f707fd4a0d9d5ebf6eecbbe8dbe6bdab1
Ben A <benjamin.anoufa@gmail.com>
2022-08-30 14:25:24 +0200

    finished

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,9 @@
-import App from 'next/app';
 import NavContext from '../context/nav-context';
 import CookieContext from '../context/cookie-context';
 import LoaderContext from '../context/loader-context';
 import Layout from '../components/layout/layout';
-import clientApi from '../libs/clientApi';
 import '../styles/globals.scss';
 import '../styles/logo.scss';
-import '../styles/font.css';
 import '../styles/scale.scss';
 
 function MyApp({ Component, pageProps }) {
