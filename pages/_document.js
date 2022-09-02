2acf08045edba3686a0753586e63fd44cf19c5e1
Ben A <benjamin.anoufa@gmail.com>
2022-09-02 08:06:07 +0200

    font weight adjustment

--- pages/_document.js
+++ pages/_document.js
@@ -6,14 +6,14 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link
-            href='https://fonts.googleapis.com/css2?family=Cormorant:wght@100;200;300;400;500;700;900&display=swap'
+            href='https://fonts.googleapis.com/css2?family=Cormorant:wght@300;400;500;700&display=swap'
             rel='stylesheet'
           />
           <link
-            href='https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;600;700&display=swap'
+            href='https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;500;700&display=swap'
             rel='stylesheet'
           />
-          <link rel="preload" href="https://use.typekit.net/fbc5oao.css" />
+          <link rel='stylesheet' href="https://use.typekit.net/fbc5oao.css" />
         </Head>
         <body>
           <div id='modal-root'></div>
