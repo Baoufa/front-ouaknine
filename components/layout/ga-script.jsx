e8b167bfe6e4a8af8bdcd13b22d30e2e88cbc896
Ben A <benjamin.anoufa@gmail.com>
2022-09-02 08:35:07 +0200

    gtag

--- components/layout/ga-script.jsx
+++ components/layout/ga-script.jsx
@@ -1,20 +1,27 @@
-import Script from 'next/script'
+import Script from 'next/script';
+import * as gtag from '../../libs/gtag';
 
 function GaScript() {
   return (
     <>
-  <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
-
-<Script strategy="lazyOnload" id="google-analytics">
-    {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-        page_path: window.location.pathname,
-        });
-    `}
-</Script>
+      <Script
+        strategy='afterInteractive'
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id='gtag-init'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
     </>
   );
 }
